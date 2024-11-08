62a8e4c37ae81fafb4d7515751c9c66164c7ae82
camelliagh <camellia.gh@gmail.com>
2024-11-08 10:55:12 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log('consoooole');
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+        
+      });
+    });
+  });
+
+
+  document.getElementById("clickMe").onclick = function () { alert('Not ready yet!'); };
